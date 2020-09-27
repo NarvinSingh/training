b53581fb7659bc16b8a0c113af9760d2726ca50c
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-09-27 01:01:33 -0400

    Feat: Intro
    Add intro script.

---
+++ shell_scripting/01_Intro.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# From https://tutorialspoint.com/unix/shell_scripting.htm
+
+#
+# Prompt the user for input and store the input in a variable
+#
+echo 'What is your name?'
+read NAME
+echo "Hello $NAME"
+
