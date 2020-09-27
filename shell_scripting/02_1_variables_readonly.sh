245b6c6b45767cbc9cfd0a806ef089151270b9a9
Narvin Singh <Narvin.A.Singh@gmail.com>
2020-09-27 03:22:13 -0400

    Feat: Variables
    Add scripts demonstrating read-only variables and unsetting variables.

---
+++ shell_scripting/02_1_variables_readonly.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# From https://www.tutorialspoint.com/unix/unix-using-variables.htm
+
+#
+# Make a variable readonly. The last line produces an error.
+#
+NAME='Narvin Singh'
+readonly NAME
+NAME='Marteena'
+
---
+++ shell_scripting/02_2_variables_unset.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+# From https://www.tutorialspoint.com/unix/unix-using-variables.htm
+
+#
+# Unset a variable. The echo statement won't print anything.
+#
+NAME='Narvin Singh'
+unset NAME
+echo $NAME
+
